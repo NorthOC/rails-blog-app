9c42789ce714c4e9a8d671bc77e46442aa4da031
Denis Lisunov <rev.denisas@gmail.com>
2021-12-27 16:32:04 +0200

    added edit and delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,26 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
   private
 
     def article_params
