941ab00e966658a41332b1007863356928e03c78
Denis Lisunov <rev.denisas@gmail.com>
2022-01-13 17:31:13 +0200

    added css to article form file + favicon

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
   
   has_many :comments, dependent: :destroy
   
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true, length: { minimum: 10 }
 end
