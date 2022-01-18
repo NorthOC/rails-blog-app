10ccdf725f42c68bc903b54f885eb75aca710bee
Denis Lisunov <rev.denisas@gmail.com>
2022-01-18 22:14:38 +0200

    added comment box styling anderror validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,24 @@ class CommentsController < ApplicationController
 
   http_basic_authenticate_with name: "cobalt", password: "cobalt", only: :destroy
 
+  def show
+    @article = Article.find(params[:article_id])
+  end
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    if @comment.save
+      redirect_to article_path(@article)
+    else
+      redirect_to article_path(@article)
+      if @comment.commenter.length < 1
+        flash[:commentNameError] = "Your Name has to be at least 1 character long."
+      end
+      if @comment.body.length < 1
+        flash[:commentBodyError] = "Your Comment has to be at least 1 character long."
+      end
+    end
   end
 
   def destroy
